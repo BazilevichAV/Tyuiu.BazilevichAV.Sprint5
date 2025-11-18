974588842b627530ead371a31a930bc20b0b1a53
BazilevichAV <Tyuiu.BazilevichAV@outlook.com>
2025-11-18 14:10:49 +0500

    Task0

--- Tyuiu.BazilevichAV.Sprint5.Task0.V14.Lib/DataService.cs
+++ Tyuiu.BazilevichAV.Sprint5.Task0.V14.Lib/DataService.cs
@@ -7,7 +7,7 @@ namespace Tyuiu.BazilevichAV.Sprint5.Task0.V14.Lib
     {
         public string SaveToFileTextData(int x)
         {
-            string path = $@"{Directory.GetCurrentDirectory()}/OutPutFileTask0.txt";
+            string path = $@"\{Directory.GetCurrentDirectory()}\OutPutFileTask0.txt";
             double z = (4 * Math.Pow(x, 3.0 / 2.0)) / (Math.Pow(x, 3.0 / 2.0) - 1);
             z = Math.Round(z, 3);
             File.WriteAllText(path, Convert.ToString(z));
