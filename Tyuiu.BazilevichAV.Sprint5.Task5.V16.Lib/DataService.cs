2da77fc726d51bdd60333269c3c9630309184ef4
BazilevichAV <Tyuiu.BazilevichAV@outlook.com>
2025-11-24 17:33:20 +0500

    Task5

--- Tyuiu.BazilevichAV.Sprint5.Task5.V16.Lib/DataService.cs
+++ Tyuiu.BazilevichAV.Sprint5.Task5.V16.Lib/DataService.cs
@@ -14,12 +14,12 @@ namespace Tyuiu.BazilevichAV.Sprint5.Task5.V16.Lib
                 string line;
                 while ((line = reader.ReadLine()) != null)
                 {
-                    string[] numbers = line.Split(' ');
+                    string[] numbers = line.Split(' ', StringSplitOptions.RemoveEmptyEntries);
                     foreach (string numStr in numbers)
                     {
                         if (int.TryParse(numStr, out int num) && num % 10 == 0)
                         {
-                            if (num > res)
+                            if (Math.Abs(num) > Math.Abs(res))
                                 res = num;
                         }
                     }
